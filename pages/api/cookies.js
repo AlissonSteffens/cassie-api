6edbb8c4f688168584a42fb55fa2a804a78d0f7d
Alisson <ali.steffens@gmail.com>
2021-06-25 10:14:19 -0300

    :tada:first version

---
+++ pages/api/cookies.js
@@ -0,0 +1,94 @@
+const url = require('url')
+const MongoClient = require('mongodb').MongoClient
+let cachedDb = null
+const requestIp = require('request-ip')
+var validate = require('jsonschema').validate;
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cachedDb
+  }
+  const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+  const client = await MongoClient.connect(uri, config)
+  const db = await client.db(url.parse(uri).pathname.substr(1))
+  cachedDb = db
+  return db
+}
+
+var schema = {
+  id: "/Cookie",
+  type: "object",
+  properties: {
+    user: { type: "string" },
+    email: { type: "string", format: "email" },
+    read: { type: "boolean" }
+  },
+  required: ["read"]
+};
+
+module.exports = async (req, res) => {
+  const db = await connectToDatabase(process.env.MONGODB_URI)
+  const collection = await db.collection('cookies')
+  const key = await db.collection('keys')
+
+  switch (req.method) {
+    case 'GET':
+      key.find({}).toArray().then(result => {
+        if (result[0].key == req.headers.auth) {
+          var limit;
+          var page;
+
+          if (req.query.limit) {
+            limit = parseInt(req.query.limit); // Make sure to parse the limit to number
+            if (limit > 10000) {
+              limit = 10000
+            }
+          } else {
+            limit = 10;
+          }
+
+          if (req.query.page) {
+            page = parseInt(req.query.page);// Make sure to parse the skip to number
+          } else {
+            page = 0;
+          }
+          collection.find({}).limit(limit).skip(limit * page).toArray().then(result => {
+            res.status(200).json(result)
+          })
+        } else {
+          res.status(401).end("Unauthorized")
+        }
+      })
+      collection.find({}).toArray().then(result => {
+        res.status(200).json(result)
+      })
+      break
+    case 'POST':
+      const data = req.body
+      data.extIp = requestIp.getClientIp(req)
+      data.time = Date.now()
+      if(data.read){
+        data.read = Boolean(data.read)
+      }
+      let validation = validate(data, schema)
+      if (validation.errors.length === 0) {
+        collection.insertOne(data).then(result => {
+          collection.findOne(result.insertedId).then(r => {
+            res.status(200).json(r)
+          })
+        })
+      }else{
+        res.status(400).json(validation.errors)
+      }
+
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
+}
+
+
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cassie API</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Essa é a API do <a href="https://www.cassiengine.com/">Cassie</a>
+        </h1>
+
+        <p className={styles.description}>
+          Como o Cassie não utiliza diretamente nenhuma conexão com banco de dados, esta API provê a ele os serviços necessários de armazenamento
+        </p>
+
+        <div className={styles.grid}>
+          <a href="/api/cookies" className={styles.card}>
+            <h3>Cookies &rarr;</h3>
+            <p>Controle de aceite para as as regras de cookies do cassie</p>
+          </a>
+
+          <a href="/api/cursos" className={styles.card}>
+            <h3>Cursos &rarr;</h3>
+            <p>Aprenda com o Portugol Studio</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.cassiengine.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Saiba mais no nosso site {' '}
+          <img src="/logo.svg" alt="Cassie Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
